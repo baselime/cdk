1997c1322289da9f71e73934cfea7dd797bc6d32
Boris Tane <boris.tane@gmail.com>
2023-08-19 20:47:20 +0100

    Fixed getting the service name for SST services

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,7 +6,8 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
 
 ## [Unreleased]
-
+### Fixed
+- Fix getting the service name for SST services
 
 ## [0.0.41] 2023-08-12
 ### Fixed
--- src/resources/alert.ts
+++ src/resources/alert.ts
@@ -2,13 +2,11 @@ import { CfnResource, Stack } from "aws-cdk-lib";
 import { Baselime as Config } from "../config";
 import { Query } from "./query";
 import { AlertProps, DeploymentAlertParameters } from "../types/alert";
-import { QueryProps } from "../types/query";
 import { getServiceName } from "../utils/service-name";
 
 export class Alert<TKey extends string> extends CfnResource {
 
 	constructor(id: string, props: AlertProps<TKey>) {
-		const stack = Stack.of(Config.getConstruct());
 		const defaultFrequency = "1hour";
 		const defaultWindow = "1hour";
 
@@ -55,7 +53,7 @@ export class Alert<TKey extends string> extends CfnResource {
 				BaselimeApiKey: Config.getApiKey(),
 				enabled: props.enabled,
 				Description: props.description,
-				Service: getServiceName(stack),
+				Service: getServiceName(),
 				Parameters,
 				Channels: props.channels || Config.getDefaultChannel() && [Config.getDefaultChannel()],
 				Origin: "cdk",
